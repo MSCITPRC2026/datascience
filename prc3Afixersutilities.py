71de078d07cff17a70d388a3c91d2e28799edd1f
MSCITPRC2026 <bhagatankit5656@gmail.com>
2026-01-29 16:51:28 +0530

    Delete prc3Afixersutilities.py

--- prc3Afixersutilities.py
+++
@@ -1,8 +0,0 @@
-import string
-import datetime as dt
-# 1 Removing leading or lagging spaces from a data entry
-print('#1 Removing leading or lagging spaces from a data entry');
-baddata = " Data Science with too many spaces is bad!!! "
-print('>',baddata,'<')
-cleandata=baddata.strip()
-print('>',cleandata,'<')
